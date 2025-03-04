13036f97cdb8c7d0af716205d3c5783cd226da8b
Witanameycom Victor <witanameycomvictor@gmail.com>
2025-03-04 08:58:07 -0300

    CartasSuperTrunfo nivel aventureiro.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,122 +1,122 @@
 #include <stdio.h>
 
-int main(){
-    // Variáveis para a carta 1
+int main() {
+    // Variáveis da primeira carta 
     char Estado[1];
     char codigo_carta1[4];
     char cidade[15];
-    int populacao;
+    float populacao;
     float area;
     float pib;
     int qtdpontosturisticos;
 
-    // Variáveis para a carta 2
+    // Variáveis da segunda carta
     char Estado2[1];
     char codigo_carta2[4];
     char cidade2[15];
-    int populacao2;
+    float populacao2;
     float area2;
     float pib2;
     int qtdpontosturisticos2;
 
     printf("Iniciando pela carta 1: \n\n");
 
-    // Escolha do estado para a carta 1
-    printf("Digite uma letra de A a H: ");
+    // Carta 1
+    printf("Digite uma letra de A a H para o estado da carta 1: ");
     scanf("%s", Estado);
     printf("Certo, a letra escolhida foi: %s\n", Estado);
 
-    // Escolha do código da carta 1
-    printf("Agora que escolheu o estado, vamos registrar o código da carta.\n");
+    printf("Agora que escolheu o estado, vamos registrar o código da carta 1.\n");
     printf("Para o código da carta, você deve Digitar uma letra seguida de números. Ex: A01, A02...\n");
-    printf("Digite o código da carta: ");
+    printf("Digite o código da carta 1: ");
     scanf("%s", codigo_carta1);
     printf("Ótimo! O código escolhido foi: %s\n\n", codigo_carta1);
 
-    // Escolha da cidade para a carta 1
-    printf("Certo, nós já temos o Estado, que é: %s, e também temos o código da carta: %s. Agora, vamos registrar a cidade.\n", Estado, codigo_carta1);
-    printf("Digite o nome da cidade: ");
+    printf("Digite o nome da cidade para a carta 1: ");
     scanf("%s", cidade);
     printf("Beleza, ótima escolha. A cidade escolhida foi: %s\n\n", cidade);
 
-    // População da cidade para a carta 1
-    printf("Certo, sabendo que a cidade escolhida foi: %s, nos diga agora a população dessa cidade.\n", cidade);
-    printf("Digite aqui qual é a população da cidade escolhida: ");
-    scanf("%i", &populacao);
-    printf("Rapaz, %s tem muita gente mesmo hein... %i, não é pouca coisa, estamos impressionados! \n\n", cidade, populacao);
+    printf("Digite a população da cidade para a carta 1: ");
+    scanf("%f", &populacao);
+    printf("Rapaz, %s tem muita gente mesmo hein... %0.3f, não é pouca coisa, estamos impressionados! \n\n", cidade, populacao);
 
-    // Área da cidade para a carta 1
-    printf("Nos informe agora, a área da cidade em km².\n");
-    printf("Área: ");
+    printf("Digite a área da cidade em km² para a carta 1: ");
     scanf("%f", &area);
     printf("Nossa, que cidade grande hein... %.2f é muita coisa!!\n\n", area);
 
-    // PIB da cidade para a carta 1
-    printf("Agora, nós queremos saber qual o PIB dessa cidade. \n");
-    printf("PIB: ");
+    printf("Digite o PIB da cidade para a carta 1: ");
     scanf("%f", &pib);
-    printf("Nossa, que cidade grande hein... %.1f é muita coisa!!\n\n", pib);
+    printf("Nossa, que cidade grande hein... %.3f é muita coisa!!\n\n", pib);
 
-    // Quantidade de pontos turísticos para a carta 1
-    printf("Agora, nos diga quantos pontos turísticos há nessa cidade.\n");
-    printf("Digite aqui quantos pontos turísticos há: ");
-    scanf("%i", &qtdpontosturisticos);
-    printf("Nossa! %s deve ter muitos turistas, né?, %i pontos turísticos, %s deve ser uma cidade bem grande...\n\n", cidade, qtdpontosturisticos, cidade);    
+    printf("Digite a quantidade de pontos turísticos para a carta 1: ");
+    scanf("%d", &qtdpontosturisticos);
+    printf("Nossa! %s deve ter muitos turistas, né?, %d pontos turísticos, %s deve ser uma cidade bem grande...\n\n", cidade, qtdpontosturisticos, cidade);
+
+    // Densidade populacional da carta 1
+    float densidade = populacao / area;
+    printf("Densidade populacional da cidade: %0.2f\n", densidade);
+
+    // PIB per capita da carta 1
+    float pibpercapita = pib / populacao;
+    printf("O PIB per capita da sua cidade é: %0.2f\n", pibpercapita);
 
     // Resumo da carta 1
     printf("Certo então vamos revisar sua carta 1: \n");
-    printf("Estado: %s\nCódigo: %s\nNome da cidade: %s\nPopulação: %i\nÁrea: %.2f\nPIB: %.1f\nQuantidade de pontos turísticos: %i\n\n", 
-           Estado, codigo_carta1, cidade, populacao, area, pib, qtdpontosturisticos);
+    printf("Estado: %s\nCódigo: %s\nNome da cidade: %s\nPopulação: %0.3f\nÁrea: %.2f\nPIB: %0.3f\nQuantidade de pontos turísticos: %d\nDensidade populacional: %0.2f\nPIB per capita: %0.2f\n\n", 
+           Estado, codigo_carta1, cidade, populacao, area, pib, qtdpontosturisticos, densidade, pibpercapita);
 
-    // Agora, iniciando a carta 2
+    // Carta 2
     printf("\nIniciando pela carta 2: \n\n");
 
     // Escolha do estado para a carta 2
-    printf("Digite uma letra de A a H: ");
+    printf("Digite uma letra de A a H para o estado da carta 2: ");
     scanf("%s", Estado2);
     printf("Certo, a letra escolhida foi: %s\n", Estado2);
 
     // Escolha do código da carta 2
-    printf("Agora que escolheu o estado, vamos registrar o código da carta.\n");
+    printf("Agora que escolheu o estado, vamos registrar o código da carta 2.\n");
     printf("Para o código da carta, você deve Digitar uma letra seguida de números. Ex: A01, A02...\n");
-    printf("Digite o código da carta: ");
+    printf("Digite o código da carta 2: ");
     scanf("%s", codigo_carta2);
     printf("Ótimo! O código escolhido foi: %s\n\n", codigo_carta2);
 
     // Escolha da cidade para a carta 2
-    printf("Certo, nós já temos o Estado, que é: %s, e também temos o código da carta: %s. Agora, vamos registrar a cidade.\n", Estado2, codigo_carta2);
-    printf("Digite o nome da cidade: ");
+    printf("Digite o nome da cidade para a carta 2: ");
     scanf("%s", cidade2);
     printf("Beleza, ótima escolha. A cidade escolhida foi: %s\n\n", cidade2);
 
     // População da cidade para a carta 2
-    printf("Certo, sabendo que a cidade escolhida foi: %s, nos diga agora a população dessa cidade.\n", cidade2);
-    printf("Digite aqui qual é a população da cidade escolhida: ");
-    scanf("%i", &populacao2);
-    printf("Rapaz, %s tem muita gente mesmo hein... %i, não é pouca coisa, estamos impressionados! \n\n", cidade2, populacao2);
+    printf("Digite a população da cidade para a carta 2: ");
+    scanf("%f", &populacao2);
+    printf("Rapaz, %s tem muita gente mesmo hein... %0.3f, não é pouca coisa, estamos impressionados! \n\n", cidade2, populacao2);
 
     // Área da cidade para a carta 2
-    printf("Nos informe agora, a área da cidade em km².\n");
-    printf("Área: ");
+    printf("Digite a área da cidade em km² para a carta 2: ");
     scanf("%f", &area2);
     printf("Nossa, que cidade grande hein... %.2f é muita coisa!!\n\n", area2);
 
     // PIB da cidade para a carta 2
-    printf("Agora, nós queremos saber qual o PIB dessa cidade. \n");
-    printf("PIB: ");
+    printf("Digite o PIB da cidade para a carta 2: ");
     scanf("%f", &pib2);
-    printf("Nossa, que cidade grande hein... %.1f é muita coisa!!\n\n", pib2);
+    printf("Nossa, que cidade grande hein... %.3f é muita coisa!!\n\n", pib2);
 
     // Quantidade de pontos turísticos para a carta 2
-    printf("Agora, nos diga quantos pontos turísticos há nessa cidade.\n");
-    printf("Digite aqui quantos pontos turísticos há: ");
-    scanf("%i", &qtdpontosturisticos2);
-    printf("Nossa! %s deve ter muitos turistas, né?, %i pontos turísticos, %s deve ser uma cidade bem grande...\n\n", cidade2, qtdpontosturisticos2, cidade2);    
+    printf("Digite a quantidade de pontos turísticos para a carta 2: ");
+    scanf("%d", &qtdpontosturisticos2);
+    printf("Nossa! %s deve ter muitos turistas, né?, %d pontos turísticos, %s deve ser uma cidade bem grande...\n\n", cidade2, qtdpontosturisticos2, cidade2);
+
+    // Densidade populacional da carta 2
+    float densidade2 = populacao2 / area2;
+    printf("Densidade populacional da cidade: %0.2f\n", densidade2);
+
+    // PIB per capita da carta 2
+    float pibpercapita2 = pib2 / populacao2;
+    printf("O PIB per capita da sua cidade é: %0.2f\n", pibpercapita2);
 
     // Resumo da carta 2
     printf("Certo então vamos revisar sua carta 2: \n");
-    printf("Estado: %s\nCódigo: %s\nNome da cidade: %s\nPopulação: %i\nÁrea: %.2f\nPIB: %.1f\nQuantidade de pontos turísticos: %i\n", 
-           Estado2, codigo_carta2, cidade2, populacao2, area2, pib2, qtdpontosturisticos2);
-    
+    printf("Estado: %s\nCódigo: %s\nNome da cidade: %s\nPopulação: %0.3f\nÁrea: %.2f\nPIB: %0.3f\nQuantidade de pontos turísticos: %d\nDensidade populacional: %0.2f\nPIB per capita: %0.2f\n", 
+           Estado2, codigo_carta2, cidade2, populacao2, area2, pib2, qtdpontosturisticos2, densidade2, pibpercapita2);
+
     return 0;
 }
